538da2494ca7420e922d49084a23fef1e7893a04
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-11 22:06:58 -0800

    subprocess call instead of run as run was throwing error

--- get_month.py
+++ get_month.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # get all of december
-DATE='2015-12-'
+DATE='2016-01-'
 for day in range(1, 32):
   if day < 10:
     date_day = DATE + '0' + str(day) + '-'
@@ -11,4 +11,4 @@ for day in range(1, 32):
   for hour in range(0,24):
     date = date_day+str(hour)
     print(date)
-    subprocess.run(['python', 'parse_gha_without_wget.py', './data/'+date])
+    subprocess.call(['python', 'parse_gha_without_wget.py', '/Volumes/WD2TB/raw_github/'+date])
